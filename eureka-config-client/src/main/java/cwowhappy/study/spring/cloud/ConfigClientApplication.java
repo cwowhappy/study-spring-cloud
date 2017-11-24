3af004f728cfa01700f33ce5a53266ae7994931e
lixiaoyi <lixiaoyi@didichuxing.com>
2017-11-24 17:03:15 +0800

    雏形

--- eureka-config-client/src/main/java/cwowhappy/study/spring/cloud/ConfigClientApplication.java
+++ eureka-config-client/src/main/java/cwowhappy/study/spring/cloud/ConfigClientApplication.java
@@ -2,7 +2,9 @@ package cwowhappy.study.spring.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class ConfigClientApplication {
     public static void main(String[] args) {
--- eureka-config-client/src/main/java/cwowhappy/study/spring/cloud/controller/ConfigController.java
+++ eureka-config-client/src/main/java/cwowhappy/study/spring/cloud/controller/ConfigController.java
@@ -1,19 +1,26 @@
 package cwowhappy.study.spring.cloud.controller;
 
+import cwowhappy.study.spring.cloud.respbody.GreetRespBody;
+import cwowhappy.study.spring.cloud.respbody.RootRespBody;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
 
 @RestController
 public class ConfigController {
     private String appName;
 
-    @RequestMapping("/app/name")
-    public String getAppName() {
-        return appName;
+    @RequestMapping("/greet")
+    public RootRespBody<GreetRespBody> greet() {
+        GreetRespBody greetRespBody = new GreetRespBody();
+        greetRespBody.setAppName(appName);
+        greetRespBody.setTimestamp(LocalDateTime.now());
+        return RootRespBody.build(RootRespBody.Status.SUCCESS, "SUCCESS", greetRespBody);
     }
 
-    @Value("${spring.application.name}")
+    @Value("${dev.spring.application.name}")
     public void setAppName(String appName) {
         this.appName = appName;
     }
